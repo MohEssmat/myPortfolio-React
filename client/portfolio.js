9f59ecab9a95d2eb2156af533d8020c36e117655
MohEssmat <92461972+MohEssmat@users.noreply.github.com>
2022-01-31 17:54:17 -0700

    added portofiliojs

---
+++ client/portfolio.js
@@ -0,0 +1,189 @@
+import emoji from "react-easy-emoji";
+
+export const greetings = {
+  name: "Mohamed Essmat",
+  title: "Hi all, I'm Mohamed Essmat",
+  description:
+    "A passionate Full Stack Web Developer with experienced in building Web applications with JavaScript / Reactjs /Nodejs/Express js/NoSQL/mySQL/Rest APIs/PWA/ Object-Oriented Programming(OOP), Bootstrap and some other cool libraries and frameworks.",
+  resumeLink: "/Essmat-Resume.pdf",
+};
+
+export const openSource = {
+  githubUserName: "MohEssmat",
+};
+
+export const contact = {};
+
+export const socialLinks = {
+  facebook: "https://www.facebook.com/mohamed.essmat.73",
+  instagram: "",
+  twitter: "",
+  github: "https://github.com/MohEssmat",
+  linkedin: "https://www.linkedin.com/in/mohamed-essmat-0b771123/",
+};
+
+export const skillsSection = {
+  title: "What I do",
+  subTitle:
+    "ENTHUSIASTICALLY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  skills: [
+    emoji("⚡ Developing front end website architecture"),
+    emoji("⚡ Designing user interactions on web pages"),
+    emoji("⚡ Developing back-end website applications"),
+    emoji("⚡ Creating servers and databases for functionality"),
+    emoji(
+      "⚡ Managing the complete software development process from conception to deployment"
+    ),
+    emoji(
+      "⚡ Overseeing and guiding the analyzing, writing, building, and deployment of software"
+    ),
+    emoji("⚡ Modifying and testing changes to previously developed programs"),
+    emoji(
+      "⚡ Managing the complete software development process from conception to deployment"
+    ),
+    emoji("⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"),
+    emoji("⚡ Ensuring responsiveness of applications"),
+    emoji("⚡ Designing and developing APIs"),
+    emoji("⚡ Develop and design RESTful services and APIs"),
+  ],
+
+  softwareSkills: [
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "vscode-icons:file-type-html",
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "vscode-icons:file-type-css",
+    },
+    {
+      skillName: "sass",
+      fontAwesomeClassname: "logos:sass",
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "logos:javascript",
+    },
+    {
+      skillName: "TypeScript",
+      fontAwesomeClassname: "logos:typescript-icon",
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "vscode-icons:file-type-reactjs",
+    },
+    {
+      skillName: "nodejs",
+      fontAwesomeClassname: "logos:nodejs-icon",
+    },
+
+    {
+      skillName: "swift",
+      fontAwesomeClassname: "vscode-icons:file-type-swift",
+    },
+    {
+      skillName: "npm",
+      fontAwesomeClassname: "vscode-icons:file-type-npm",
+    },
+    {
+      skillName: "sql-database",
+      fontAwesomeClassname: "vscode-icons:file-type-sql",
+    },
+    {
+      skillName: "mongoDB",
+      fontAwesomeClassname: "vscode-icons:file-type-mongo",
+    },
+
+    {
+      skillName: "git",
+      fontAwesomeClassname: "logos:git-icon",
+    },
+  ],
+};
+
+export const SkillBars = [
+  {
+    Stack: "Frontend/Design", //Insert stack or technology you have experience in
+    progressPercentage: "90", //Insert relative proficiency in percentage
+  },
+  {
+    Stack: "Backend",
+    progressPercentage: "80",
+  },
+  {
+    Stack: "Programming",
+    progressPercentage: "80",
+  },
+];
+
+export const educationInfo = [
+  {
+    schoolName: "University of Denver",
+    subHeader: "Full Stack Web Developer Boot Camp",
+  },
+  {
+    schoolName: "Cairo University",
+    subHeader: "Bachelor of business",
+  },
+];
+
+export const experience = [
+  {
+    role: "Full-Stack Web Developer",
+    company: "Freelance - Self Employed",
+    // companylogo: "",
+    date: "April 2020 –Present",
+    desc: "Create commercial websites for several clients  - Wrote structured, tested, readable, and maintainable code - Developed code following common security practices and development design patterns -  Designed and developed 10+ front-end and back-end applications utilizing JavaScript, React, Node.js, and TypeScript- Participated in the planning and execution of 10+ business continuity/disaster recovery exercises - Developed code following common security practices and development design patterns",
+  },
+  {
+    role: "Project Document Manager",
+    company: "thyssenkrupp Industrial Solutions USA Inc",
+    // companylogo: "",
+    date: "November 2008 – March 2020",
+    desc: "Working on SAP program to manage the document flow between projects - Contract Set-up: Setting up new Contracts for all projects in SAP- - Maintain document control registers / documents for incoming and outgoing project document",
+  },
+];
+
+export const projects = [
+  {
+    name: "Watch-iT Movie App",
+    desc: "Watch-iT application is a movie app. It pulls movies from The Movie Database using API calls. Users have the option to view the Now Playing, Upcoming, Top Rated and Popular movies, Sort them by genre or search for other movies using the search feature, And get back information about each movie they want to watch, including a trailer! Sign up to create a list of your favorites!",
+    github: "https://github.com/elliscj/watchIT",
+    link: "https://thawing-fjord-57482.herokuapp.com/",
+  },
+  {
+    name: "uProjekt Project Calendar",
+    desc: "Programmers require a responsive online planner for scheduling timeframes to complete their projects: uProjekt. The planner will be allocated based on a project's deadline, with Typical, Accelerated, and Custom options available. Currently these features are in a placeholder stage.",
+    github: "https://github.com/MohEssmat/uProjekt",
+    link: "https://mohessmat.github.io/uProjekt/",
+  },
+  {
+    name: "Lit Custom Shop",
+    desc: "Lit custom shop is an e-commerce website that allows users to custom, upload and design their designs/logo/image, It's print on demand website that allows the customers to print on any material",
+
+    link: "https://www.litcustomshop.com/",
+  },
+  {
+    name: "Wolfamaze",
+    desc: "Wolfamaze is an e-commerce website for wolves and lover products, It's custom jewelry, clothes, bedsits, and others with wolves designs",
+    link: "https://wolfamaze.com/",
+  },
+  {
+    name: "View More Project",
+
+    link: "https://github.com/MohEssmat",
+  },
+];
+
+export const feedbacks = [
+  {
+    name: "Hassan Mahmoud",
+    feedback:
+      "We have been very happy with our new website! It looks professional and very easy to navigate. Our experience with the customer service at Mohamed Essmat has been great. They handle things very efficiently and are available for any questions we have. They also keep us updated on daily reports so we know how our site is doing. I would recommend that you have choose Mohamed web developer services for you",
+  },
+  {
+    name: "Maheen Altaf",
+    feedback:
+      "The website was very Professional , everything was perfectly made as was requested . it loads quickly, with no waiting. the quality and attention to details were very amazing. our clients have been using the site and enjoy all the new features. Excellent job and highly recommend ",
+  },
+];
